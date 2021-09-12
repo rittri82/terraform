42a01655a3561180f02000d643a62d053ec31f78
rittri82 <82996039+rittri82@users.noreply.github.com>
2021-09-12 16:05:16 +0530

    commit

--- c5-ec2instance.tf
+++ c5-ec2instance.tf
@@ -1,7 +1,7 @@
 # EC2 Instance
 
 resource "aws_instance" "myec2vm" {
-  ami = "data.aws_ami.amzlinux2.id"
+  ami = data.aws_ami.amzlinux2.id
   instance_type = var.instance_type
   user_data = file("${path.module}/app1-install.sh")
   key_name = var.instance_keypair
