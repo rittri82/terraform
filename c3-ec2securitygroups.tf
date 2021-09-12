227bec849946f5944bf09a78faabebe8376d8478
rittri82 <82996039+rittri82@users.noreply.github.com>
2021-09-12 13:23:32 +0530

    commit

--- c3-ec2securitygroups.tf
+++ c3-ec2securitygroups.tf
@@ -4,26 +4,22 @@ resource "aws_security_group" "vpc-ssh" {
   name        = "vpc-ssh"
   description = "Dev VPC SSH"
 
-  ingress = [
-    {
+  ingress     {
       description      = "Allow port 22"
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
     }
-  ]
-
-  egress = [
-    {
+  
+  egress     {
       description      = "Allow all outbound"  
       from_port        = 0
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
-      
-    }
-  ]
+  }
+  
 
   tags = {
     Name = "vpc-ssh-sg"
@@ -37,26 +33,24 @@ resource "aws_security_group" "vpc-web" {
   name        = "vpc-web"
   description = "Dev VPC Web"
 
-  ingress = [
-    {
+  ingress     {
       description      = "Allow port 80"
       from_port        = 80
       to_port          = 80
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
-    }
-  ]
+  }
+  
 
-  egress = [
-    {
+  egress     {
       description      = "Allow all outbound"  
       from_port        = 0
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
-      
-    }
-  ]
+  
+  }
+  
 
   tags = {
     Name = "vpc-web-sg"
