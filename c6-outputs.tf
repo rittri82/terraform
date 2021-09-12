6c67faa0128056b8a8bd4158583f8ba3d08f0d86
rittri82 <82996039+rittri82@users.noreply.github.com>
2021-09-12 18:33:20 +0530

    commit

--- c6-outputs.tf
+++ c6-outputs.tf
@@ -9,13 +9,13 @@ output "for_output_list" {
 # For loop with Map
 output "for_output_map1" {
   description = "For Loop with Map1"
-  value       = [for instance in aws_instance.myec2vm: instance.id => instance.public_dns]
+  value       = {for instance in aws_instance.myec2vm: instance.id => instance.public_dns}
 }
 
 # For loop with Map Advanced
 output "for_output_map2" {
   description = "For Loop with Map2"
-  value       = [for c,  in aws_instance.myec2vm: c => instance.public_dns]
+  value       = {for c,  in aws_instance.myec2vm: c => instance.public_dns}
 }
 
 # Output with legacy splat operator
