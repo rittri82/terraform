e961d6e78bc8a8dd8ddb85c6bcdf3da6ffcf699f
rittri82 <82996039+rittri82@users.noreply.github.com>
2021-09-12 18:43:30 +0530

    commit

--- c5-ec2instance.tf
+++ c5-ec2instance.tf
@@ -3,8 +3,8 @@
 resource "aws_instance" "myec2vm" {
   ami = data.aws_ami.amzlinux2.id
   # instance_type = var.instance_type
-  instance_type = var.instance_type_list[0] # for list
-  # instance_type = var.instance_type_map["prod"] # for map
+  # instance_type = var.instance_type_list[0] # for list
+  instance_type = var.instance_type_map["prod"] # for map
   user_data = file("${path.module}/app1-install.sh")
   key_name = var.instance_keypair
   vpc_security_group_ids = [
